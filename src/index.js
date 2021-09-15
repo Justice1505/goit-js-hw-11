9c54ce9cf2360663f36488ac34498ac6a9db9903
Alexander Osadchuk <85343271+Justice1505@users.noreply.github.com>
2021-09-15 13:36:27 +0300

    updates

--- src/index.js
+++ src/index.js
@@ -12,21 +12,21 @@ import ScrollToTopBtn from './js/scrollBtn';
 import throttle from 'lodash.throttle';
 
 const refs = {
-    searchForm: document.querySelector('#search-form'),
-    gallery: document.querySelector('.js-gallery'),
-    scrollToTopBtn: document.querySelector('.scrollToTopBtn'),
+  searchForm: document.querySelector('#search-form'),
+  gallery: document.querySelector('.js-gallery'),
+  scrollToTopBtn: document.querySelector('.scrollToTopBtn'),
 };
 
 const imageApiService = new ImagesApiService();
 const loadMoreBtn = new LoadMoreBtn({
-    selector: '.js-load-more-btn',
-    hidden: true,
+  selector: '.js-load-more-btn',
+  hidden: true,
 });
 const scrollToTopBtn = new ScrollToTopBtn('.scrollToTopBtn');
 const lightbox = new SimpleLightbox('.js-gallery a');
 const scrollToNewImages = new ScrollToNewImages({
-    selector: '.js-gallery .gallery__item',
-    imagesPerPage: imageApiService.perPage,
+  selector: '.js-gallery .gallery__item',
+  imagesPerPage: imageApiService.perPage,
 });
 
 refs.searchForm.addEventListener('submit', onSearchFormSubmit);
@@ -35,68 +35,68 @@ document.addEventListener('scroll', throttle(scrollToTopBtn.onScroll.bind(scroll
 refs.scrollToTopBtn.addEventListener('click', scrollToTopBtn.onScrollToTopBtnClick);
 
 async function onSearchFormSubmit(e) {
-    e.preventDefault();
+  e.preventDefault();
 
-    const searchQuery = e.currentTarget.elements.query.value.trim();
-    if (searchQuery === '') {
-        Notiflix.Notify.failure('Enter something for query');
-        return;
+  const searchQuery = e.currentTarget.elements.query.value.trim();
+  if (searchQuery === '') {
+    Notiflix.Notify.failure('Enter something for query');
+    return;
+  }
+
+  imageApiService.query = searchQuery;
+  imageApiService.resetPage();
+  loadMoreBtn.hide();
+  clearMrkInside(refs.gallery);
+  scrollToNewImages.resetNumberItemToScroll();
+
+  try {
+    const { hits, totalHits } = await imageApiService.fetchImages();
+
+    if (imageApiService.isLastPage) {
+      Notiflix.Notify.failure(
+        'Sorry, there are no images matching your search query. Please try again.',
+      );
+      return;
     }
 
-    imageApiService.query = searchQuery;
-    imageApiService.resetPage();
-    loadMoreBtn.hide();
-    clearMrkInside(refs.gallery);
-    scrollToNewImages.resetNumberItemToScroll();
-
-    try {
-        const { hits, totalHits } = await imageApiService.fetchImages();
-
-        if (imageApiService.isLastPage) {
-            Notiflix.Notify.failure(
-                'Sorry, there are no images matching your search query. Please try again.',
-            );
-            return;
-        }
-
-        updateUIAndRefreshLightbox(hits);
-        loadMoreBtn.show();
-        Notiflix.Notify.success(`Hooray! We found ${totalHits} images.`);
-    } catch (error) {
-        errorNotify(error.message);
-    }
+    updateUIAndRefreshLightbox(hits);
+    loadMoreBtn.show();
+    Notiflix.Notify.success(`Hooray! We found ${totalHits} images.`);
+  } catch (error) {
+    errorNotify(error.message);
+  }
 }
 
 async function onLoadMoreBtnClick() {
-    loadMoreBtn.disable();
+  loadMoreBtn.disable();
 
-    try {
-        const { hits } = await imageApiService.fetchImages();
+  try {
+    const { hits } = await imageApiService.fetchImages();
 
-        if (imageApiService.isLastPage) {
-            Notiflix.Notify.info(`We're sorry, there are no more posts to load`);
-            loadMoreBtn.hide();
-            loadMoreBtn.enable();
-            return;
-        }
-
-        updateUIAndRefreshLightbox(hits);
-        loadMoreBtn.enable();
-        scrollToNewImages.scroll();
-    } catch (error) {
-        errorNotify(error.message);
+    if (imageApiService.isLastPage) {
+      Notiflix.Notify.info(`We're sorry, there are no more posts to load`);
+      loadMoreBtn.hide();
+      loadMoreBtn.enable();
+      return;
     }
+
+    updateUIAndRefreshLightbox(hits);
+    loadMoreBtn.enable();
+    scrollToNewImages.scroll();
+  } catch (error) {
+    errorNotify(error.message);
+  }
 }
 
 function appendPhotoCardsMarkup(data) {
-    refs.gallery.insertAdjacentHTML('beforeend', galleryItemsMrk(data));
+  refs.gallery.insertAdjacentHTML('beforeend', galleryItemsMrk(data));
 }
 
 function updateUIAndRefreshLightbox(data) {
-    appendPhotoCardsMarkup(data);
-    lightbox.refresh();
+  appendPhotoCardsMarkup(data);
+  lightbox.refresh();
 }
 
 function errorNotify(message) {
-    Notiflix.Notify.warning(`Something went wrong (${message}) please try again later`);
+  Notiflix.Notify.warning(`Something went wrong (${message}) please try again later`);
 }
