eb2e3783af64985107cdfe971f0d40271e10bb4d
Alexander Osadchuk <85343271+Justice1505@users.noreply.github.com>
2021-09-15 12:35:02 +0300

    some changes

--- src/js/refs.js
+++
@@ -1,7 +0,0 @@
-const refs = {
-    searchForm: document.querySelector('#search-form'),
-    gallery: document.querySelector('.js-gallery'),
-    scrollToTopBtn: document.querySelector('.scrollToTopBtn'),
-};
-
-export default refs;
